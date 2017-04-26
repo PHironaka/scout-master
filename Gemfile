93c135cd4164da8ebcabcb6da2c315a44964f33e
PHironaka <peterhironaka@mac.com>
2017-04-25 23:29:45 -0700

    update the paperclip migration to this new test app

--- Gemfile
+++ Gemfile
@@ -13,7 +13,6 @@ gem 'rails', '~> 5.0.2'
 gem 'bcrypt',         '3.1.11'
 gem 'bootstrap-sass', '3.3.6'
 # Use sqlite3 as the database for Active Record
-gem 'pg', '0.18.4'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
@@ -53,6 +52,10 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+group :production do
+  gem 'pg', '0.18.4'
 end
 
 group :test do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426060711) do
+ActiveRecord::Schema.define(version: 20170426062805) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -26,8 +26,12 @@ ActiveRecord::Schema.define(version: 20170426060711) do
     t.string   "title"
     t.string   "body"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_locations_on_user_id"
   end
 
