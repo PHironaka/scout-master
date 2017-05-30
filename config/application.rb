e9019698147817570f0155a89048938282264e28
PHironaka <peterhironaka@mac.com>
2017-05-30 13:40:09 -0700

    removed config file from application rb

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.exceptions_app = self.routes
 
 module ScoutMaster
   class Application < Rails::Application
