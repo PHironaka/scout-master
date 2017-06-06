6f52d000af00170e6e0f9c3197322164c02df0cc
Peter HIronaka <peter.hironaka@OMMCXXYY1234-2.local>
2017-06-06 14:57:42 -0700

    modifications to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => "https://agile-inlet-60248.herokuapp.com/" }
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
@@ -12,6 +13,10 @@ Rails.application.configure do
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
    }
+
+  config.action_mailer.default_url_options = {:host => "localhost:3000"}
+
+  config.action_mailer.perform_caching = false
   # Code is not reloaded between requests.
   config.cache_classes = true
 
