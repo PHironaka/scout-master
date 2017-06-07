771adda33ac0cf36641bd7f944d6399cf64a286e
PHironaka <peterhironaka@mac.com>
2017-06-07 08:30:12 -0700

    update .env and production details

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,14 +88,14 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'agile-inlet-60248.herokuapp.com' }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '25',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => ENV["SENDGRID_DOMAIN"],
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: 587,
+    domain: ENV["SENDGRID_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"]
+   }
 
 end
