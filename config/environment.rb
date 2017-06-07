9f1d13e41514f77b422990ee08c6095159959b92
Peter HIronaka <peter.hironaka@OMMCXXYY1234-2.local>
2017-06-07 10:59:34 -0700

    update environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Configuration for using SendGrid on Heroku
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => "yourSendGridusernameyougetfromheroku",
+  :password => "yourSendGridpasswordyougetfromheroku",
+  :domain => "staging.freelanceful.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
