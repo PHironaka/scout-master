b80a7690d8a30ed71de29f8e0f5a7f3ce6444560
Peter HIronaka <peter.hironaka@OMMCXXYY1234-2.local>
2017-06-07 12:49:36 -0700

    update host in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'agile-inlet-60248.herokuapp.com'
+  host = 'agile-inlet-60248'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
