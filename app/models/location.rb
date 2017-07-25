69d9523c23bce5e14de85e90257e44f2c9ce7d58
PHironaka <peterhironaka@mac.com>
2017-07-24 22:52:56 -0700

    updated styling for maps, removed scrollwheel and coloring

--- app/models/location.rb
+++ app/models/location.rb
@@ -5,7 +5,6 @@ class Location < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-
   def should_generate_new_friendly_id?
     new_record?
   end
@@ -33,6 +32,9 @@ class Location < ApplicationRecord
     }
   end
 
+  def gmaps4rails_address
+   address
+  end
 
 
    acts_as_taggable
